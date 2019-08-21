83e9d59f3ce27703dca6a1fdf896793ea0790b8c
Alexander Vereeken <37543796+Alexander88207@users.noreply.github.com>
2019-08-21 13:17:59 +0200

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -29,10 +29,12 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
 
-        1) echo arch deps
+        1)
+echo -e "\e[40;38;5;82m Installing dependencies \e[30;48;5;82m\e[0m"
+sudo pacman -S bc git linux-headers
             ;;
         2) 
-echo -e "\e[40;38;5;82m Installing dependencies for solus \e[30;48;5;82m\e[0m"
+echo -e "\e[40;38;5;82mInstalling dependencies for solus\e[30;48;5;82m\e[0m"
 sudo eopkg it gcc binutils git make
 HEIGHT=15
 WIDTH=40
@@ -61,14 +63,23 @@ sudo eopkg it linux-current-headers
 sudo eopkg it linux-lts-headers
 esac
             ;;
-        3) echo ubuntu
+        3)
+echo -e "\e[40;38;5;82m Installing dependencies \e[30;48;5;82m\e[0m"
+sudo apt install -y git build-essential make autoconf libtool gcc gettext
             ;;
-        4) echo Sabayon 
+        4)
+echo -e "\e[40;38;5;82m Installing dependencies\e[30;48;5;82m\e[0m" 
+sudo equo install gcc --ask
             ;;
-        5) echo opensuse
+        5)
+echo -e "\e[40;38;5;82mInstalling dependencies\e[30;48;5;82m\e[0m"  
+sudo zypper install make kernel-source
             ;;
-        6) echo uninstallation
+        6) echo uninstallation is coming soon again
+bash
             ;;
+        7) echo reinstallation is coming soon again
+bash
 esac
 #!/bin/bash
 HEIGHT=15
@@ -95,23 +106,73 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
         1)
-            ;;
-        2)
-            ;;
-        3)
-            ;;
-        4)
-            ;;
-        5)
-echo -e "\e[40;38;5;82m Downloading driver \e[30;48;5;82m\e[0m"
-git clone https://github.com/ulli-kroll/rtl8188fu
-cd rtl8188fu
+echo -e "\e[40;38;5;82mDownloading driver\e[30;48;5;82m\e[0m"
+git clone https://github.com/gordboy/rtl8812au
+cd rtl8812au
 echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
 make
 echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
 sudo make install
+sudo modprobe 8812au
+echo -e "\e[40;38;5;82mDone :) \e[30;48;5;82mYou can now use your wifi adapter!\e[0m"
+cd /home/$USER/RTL-Driver-Installer-Files
+rm -d -r rtl8812au
+git clone https://github.com/gordboy/rtl8812au
+            ;;
+        2)
+echo -e "\e[40;38;5;82mDownloading driver\e[30;48;5;82m\e[0m"
+git clone https://github.com/quickreflex/rtl8188eus
+cd rtl88eus
+echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
+make all
+echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+sudo make install
+sudo modprobe 8188eu
+echo -e "\e[40;38;5;82m Done :) \e[30;48;5;82m You can now use your wifi adapter! \e[0m"
+echo -e "\e[40;38;5;82mCleanup & make the driver ready for an reinstallation\e[30;48;5;82m\e[0m"
+cd /home/$USER/RTL-Driver-Installer-Files
+rm -d -r rtl88eus
+git clone https://github.com/quickreflex/rtl8188eus
+            ;;
+        3)
+echo -e "\e[40;38;5;82mDownloading driver\e[30;48;5;82m\e[0m"
+git clone https://github.com/tomaspinho/rtl8821ce
+cd rtl8821ce
+echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
+make
+echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+sudo make install
+sudo modprobe 8821ce
+echo -e "\e[40;38;5;82mDone :) \e[30;48;5;82mYou can now use your wifi adapter!\e[0m"
+cd /home/$USER/RTL-Driver-Installer-Files
+rm -d- -r rtl8821ce
+git clone https://github.com/tomaspinho/rtl8821ce
+            ;;
+        4)
+echo -e "\e[40;38;5;82mDownloading driver\e[30;48;5;82m\e[0m"
+git clone git clone https://github.com/smlinux/rtl8723de.git
+cd rtl8723de
+echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
+make
+echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+sudo make install
+sudo modprobe 8723de
+echo -e "\e[40;38;5;82mDone :) \e[30;48;5;82mYou can now use your wifi adapter!\e[0m"
+cd /home/$USER/RTL-Driver-Installer-Files
+rm -d- -r rtl8723de
+git clone https://github.com/smlinux/rtl8723de.git
+            ;;
+        5)
+echo -e "\e[40;38;5;82mDownloading driver\e[30;48;5;82m\e[0m"
+git clone https://github.com/ulli-kroll/rtl8188fu
+cd rtl8188fu
+echo -e "\e[40;38;5;82mBuilding driver\e[30;48;5;82m\e[0m"
+make
+echo -e "\e[40;38;5;82m Installing driver \e[30;48;5;82m\e[0m"
+sudo make install
 sudo modprobe cfg80211
-echo -e "\e[40;38;5;82m Cleanup & make the driver ready for an reinstallation \e[30;48;5;82m\e[0m"
+echo -e "\e[40;38;5;82mCleanup & make the driver ready for an reinstallation\e[30;48;5;82m\e[0m"
+cd /home/$USER/RTL-Driver-Installer-Files
 rm -d -r rtl8188fu
 git clone https://github.com/ulli-kroll/rtl8188fu
 esac
