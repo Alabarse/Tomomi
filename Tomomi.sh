e1316d62859dfdb3e43f0b69048e60d877c40758
Alexander Vereeken <37543796+Alexander88207@users.noreply.github.com>
2020-04-15 18:35:56 +0200

    Update Tomomi.sh

--- Tomomi.sh
+++ Tomomi.sh
@@ -49,7 +49,7 @@ DRV=$(zenity --list --radiolist --height=300 --width 200 --title="$NAME $VER" --
 PASSWORD=$(zenity --password --title "The Script will now install the driver $DRV, enter your password to proceed")
 
 if [[ $DRV == *"RTL8812au"* ]]; then
-git clone https://github.com/gordboy/rtl8812au-5.6.4.2
+git clone https://github.com/gordboy/rtl8812au-5.6.4.2.git
 cd rtl8812au-5.6.4.2
 echo -e "\e[40;38;5;82m Building driver \e[30;48;5;82m\e[0m"
 make
